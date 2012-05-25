d4023a038005d5fb0079f7d66cea80e9a9e92918
Jacob Rosenthal <jakerosenthal@gmail.com>
2012-05-24 23:26:46 -0700

    cleaned up css and js files into directories

--- head.php
+++ head.php
@@ -2,10 +2,10 @@
 <meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
 <meta name="description" content="Official website of Jacob Rosenthal: HeatSync Labs director, Starcraft addict, and general purveyor of good taste." />
 <meta name="keywords" content="jacob rosenthal augmentous citizen gadget ubiquitous computing body modification hardware engineer open source heatsync labs" />
-<link rel="StyleSheet" href="default.css" type="text/css" />
-<script type="text/javascript" src="tinybox.js"></script>
-<link rel="StyleSheet" href="overflow-touch.css" type="text/css" />
-<script type="text/javascript" src="overflow-touch.js"></script>
+<link rel="StyleSheet" href="css/default.css" type="text/css" />
+<script type="text/javascript" src="js/tinybox.js"></script>
+<link rel="StyleSheet" href="css/overflow-touch.css" type="text/css" />
+<script type="text/javascript" src="js/overflow-touch.js"></script>
 <script src="js/patternizer.min.js"></script>
 <script src="js/jquery.ba-throttle-debounce.min.js"></script>
 <link href="http://fonts.googleapis.com/css?family=Jura&subset=latin" rel="stylesheet" type="text/css">
